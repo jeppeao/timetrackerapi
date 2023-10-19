8907e00573439b59de2d944c1691c30c2b2a2f75
jeppeao <jeppeao@gmail.com>
2023-10-19 22:39:41 +0200

    added basic express server and vercel.json

--- build/api/index.js
+++ build/api/index.js
@@ -1,2 +1,9 @@
-"use strict";
-console.log("hello world");
+import express from 'express';
+var app = express();
+app.get('/', function (req, res, next) {
+    res.status(200).send('Hello World!');
+});
+app.listen(3000, function () {
+    console.log("Example app listening on port 3000");
+});
+console.log("hello");
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+const app = express ();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send('Hello World!');
+})
+
+exports = {
+  app
+}
+
+console.log("hello")
