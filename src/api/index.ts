637da9df0de5c89054387ec65f31e423ecb63221
jeppeao <jeppeao@gmail.com>
2023-10-19 23:07:01 +0200

    fixed export statement syntax

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
+
+
 const app = express ();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send('Hello World!');
 })
 
-exports = {
+export {
   app
 }
 
