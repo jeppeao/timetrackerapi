dff2b8056b798aebe9604805f029415f18db848a
jeppeao <jeppeao@gmail.com>
2023-10-22 18:47:52 +0200

    removed all traces of vercel settings

--- build/index.js
+++ build/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 var app = express();
+var PORT = 3000;
 app.get('/', function (req, res, next) {
     res.status(200).send('Hello World!');
 });
-exports = {
-    app: app
-};
-console.log("hello");
+app.listen(PORT, function () {
+    console.log("Listening on port ".concat(PORT));
+});
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 
 
 const app = express ();
+const PORT = 3000;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send('Hello World!');
 })
 
-export {
-  app
-}
 
-console.log("hello")
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
